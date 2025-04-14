353adfc46da915e024f87ce2155db4d7fd83df81
SethW4 <69054856+SethW4@users.noreply.github.com>
2025-04-14 11:16:18 -0400

    Accidentally only used the first SBox. Fixed

--- desLarge.py
+++ desLarge.py
@@ -82,7 +82,7 @@ def DES(mode, inputVal, argKey):
     initPerm = [58,50,42,34,26,18,10,2,60,52,44,36,28,20,12,4,62,54,46,38,30,22,14,6,64,56,48,40,32,24,16,8,57,49,41,33,25,17,9,1,59,51,43,35,27,19,11,3,61,53,45,37,29,21,13,5,63,55,47,39,31,23,15,7] 
     invPerm = [40,8,48,16,56,24,64,32,39,7,47,15,55,23,63,31,38,6,46,14,54,22,62,30,37,5,45,13,53,21,61,29,36,4,44,12,52,20,60,28,35,3,43,11,51,19,59,27,34,2,42,10,50,18,58,26,33,1,41,9,49,17,57,25] 
     eBox = [32,1,2,3,4,5,4,5,6,7,8,9,8,9,10,11,12,13,12,13,14,15,16,17,16,17,18,19,20,21,20,21,22,23,24,25,24,25,26,27,28,29,28,29,30,31,32,1]
-    
+    #print(len(eBox))
     
     permKey = [0 for i in range(56)] 
     for i in range(56): 
@@ -226,8 +226,11 @@ def DES(mode, inputVal, argKey):
             sColAdd = int(colStr, 2)
             sRowAdd = int(rowStr, 2)
             #print(sRowAdd, sColAdd)
-            
-            val1 = S0[sRowAdd][sColAdd] 
+            if(side):
+                val1 = S0[sRowAdd][sColAdd] 
+            else:
+                val1 = S1[sRowAdd][sColAdd] 
+
             S01 = [0,0,0,0] 
             S01 = [0 if char == '0' else 1 for char in str(bin(val1))[2:]]
             while(len(S01) < 4):
@@ -295,7 +298,11 @@ def DES(mode, inputVal, argKey):
             sRowAdd = int(rowStr, 2)
             #print(sRowAdd, sColAdd)
             
-            val1 = S0[sRowAdd][sColAdd] 
+            if(side):
+                val1 = S0[sRowAdd][sColAdd]
+            else:
+                val1 = S1[sRowAdd][sColAdd]
+
             S01 = [0,0,0,0] 
             S01 = [0 if char == '0' else 1 for char in str(bin(val1))[2:]]
             while(len(S01) < 4):
@@ -365,11 +372,14 @@ def tripleDES(mode, message, key1, key2, key3):
         
 tempKey = "".join(["0" for i in range(64)])
 #print(DES(0,"00011101111010101010111011011","1100011110"))    
-#print(DES(0,"00011101111010101010111011011",tempKey))    
+print(DES(0,"00011101111010101010111011011",tempKey))    
 #1100101011100001110011111110110111000110111011010010010101000010
-#print(DES(1,"1100101011100001110011111110110111000110111011010010010101000010",tempKey))   
+print(DES(1,"1001101100010101110110100111100100100011010010000110000000000110",tempKey))   
 #0001110111101010101011101101100000000000000000000000000000000000 
 #00011101111010101010111011011
+
+
+
 
 # 00011101111010101010111011011 <-- input 
 # 00011101111010101010111011011000 <-- encrypted and then decrypted again, so it seems to work 
